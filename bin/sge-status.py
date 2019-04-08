9adcd1b0112df5e717cb439ad7c97b78d16e2bbf
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2019-04-08 15:43:40 +0200

    pipeline worked on GTDB dataset

--- bin/sge-status.py
+++ bin/sge-status.py
@@ -5,13 +5,9 @@ import re
 import subprocess
 from subprocess import Popen, PIPE
 
-# setup
-jobid = sys.argv[1]
-regex = re.compile(r' +')
 
-# checking for running job with qstat
-try:
-    # checking qstat
+#-- functions --#
+def qstat_check(jobid, regex):
     p = Popen(['qstat'], stdout=PIPE)
     output, err = p.communicate()
     for x in output.decode().split('\n'):
@@ -23,15 +19,19 @@ try:
                 print('failed')
             else:
                 print('running')
+            p.stdout.close()
             exit(0)
-    # if not listed via qstat, parsing sge accounting info
-    time.sleep(1)   # in case the job hasn't been written to the account yet
+
+def qacct_check(jobid, max_lines):
     acct_file = '/var/lib/gridengine/default/common/accounting'
     cmd = 'tac {acct_file} | awk -F: -v id={jobid}'.format(acct_file=acct_file, jobid=jobid)
     cmd += " '{if ($6 == id) {print $0; exit 0}}'"
     p = Popen([cmd], stdout=PIPE, shell=True)
     output, err = p.communicate()
-    for x in output.decode().split('\n'):
+    for i,x in enumerate(output.decode().split('\n')):
+        if i > max_lines:
+            p.stdout.close()
+            break
         y = x.split(':')
         if len(y) < 12:
             continue
@@ -40,8 +40,21 @@ try:
                 print('success')
             else:
                 print('failed')
+            p.stdout.close()
             exit(0)
     print('running')
+
+
+#-- main --#
+jobid = sys.argv[1]
+regex = re.compile(r' +')
+try:
+    # checking qstat
+    qstat_check(jobid, regex)
+    # if not listed via qstat, parsing sge accounting info
+    #time.sleep(1)   # in case the job hasn't been written to the account yet
+    qacct_check(jobid, 10000)
+    
 except KeyboardInterrupt:
     print("failed")
 
